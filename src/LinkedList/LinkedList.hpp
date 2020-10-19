260221532a78aea9da85fd82605920484a2e0625
El Strategico <strategico.dev@gmail.com>
2020-10-20 02:01:08 +0800

    Added methods: swap, removeDuplicates

--- src/LinkedList/LinkedList.hpp
+++ src/LinkedList/LinkedList.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <map>
+
 #include <stdint.h>
 
 #include <LinkedList/ListNode.hpp>
@@ -26,12 +28,17 @@ namespace ns
 		T operator[](int index);
 
 		void insert(T value);
+		void insert(T value, int index);
 
 		void reverse();
 
 		void replace(int index, T value);
 
+		void swap(ListNode<T>* x, ListNode<T>* y);
+		void swap(int xIndex, int yIndex);
+
 		void remove(int index);
+		int removeDuplicates();
 		void clear();
 	};
 
@@ -112,6 +119,26 @@ namespace ns
 	}
 
 	template<typename T>
+	inline void LinkedList<T>::insert(T value, int index)
+	{
+		if (index < 0 || index >= getSize())
+		{
+			insert(value);
+			return;
+		}
+
+		ListNode<T>* node = find(index);
+		ListNode<T>* insertable = new ListNode<T>(value, node->prev, node);
+
+		if (node->prev == nullptr) head = insertable;
+		else node->prev->next = insertable;
+
+		node->prev = insertable;
+
+		size++;
+	}
+
+	template<typename T>
 	inline void LinkedList<T>::reverse()
 	{
 		ListNode<T>* current = tail;
@@ -140,6 +167,23 @@ namespace ns
 	}
 
 	template<typename T>
+	inline void LinkedList<T>::swap(ListNode<T>* x, ListNode<T>* y)
+	{
+		T tmp = x->value;
+		x->value = y->value;
+		y->value = tmp;
+	}
+
+	template<typename T>
+	inline void LinkedList<T>::swap(int xIndex, int yIndex)
+	{
+		ListNode<T>* x = find(xIndex);
+		ListNode<T>* y = find(yIndex);
+
+		swap(x, y);
+	}
+
+	template<typename T>
 	inline void LinkedList<T>::remove(int index)
 	{
 		ListNode<T>* removable = find(index);
@@ -153,6 +197,30 @@ namespace ns
 
 		size--;
 		delete removable;
+	}
+
+	template<typename T>
+	inline int LinkedList<T>::removeDuplicates()
+	{
+		std::map<T, int> table;
+		int removed = 0;
+
+		for (int i = 0; i < getSize(); i++)
+		{
+			ListNode<T>* current = find(i);
+
+			if (table.find(current->value) != table.end())
+			{
+				remove(i);
+				i--;
+				removed++;
+				continue;
+			}
+
+			table[current->value] = 1;
+		}
+
+		return removed;
 	}
 
 	template<typename T>
